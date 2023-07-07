e1eec24b83cb2fe2b7ebc6d3faec2c52415f1451
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2023-07-07 17:22:59 +0200

    initial commit

---
+++ deel/libname/fake.py
@@ -0,0 +1,35 @@
+"""
+This will be the module description. It will be automatically generated properly
+with mkdocs.
+Do not forget to follow
+[Google docstrings convention](https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html) format.
+"""
+import numpy as np
+
+def hello_world():
+    """
+    A method that print "Hello World!".
+
+    Note:
+        This method do not requires any parameters.
+    """
+    print("Hello World!")
+
+def addition(a: float, b: float):
+    """
+    A method that performs the following math:
+
+    $$
+    c = a + b
+    $$
+
+    Args:
+        a (float): A float number
+        b (float): Another float number
+    
+    Returns:
+        float: The sum of a and b
+    """
+    c = np.sum([a, b])
+
+    return c
