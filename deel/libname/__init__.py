87a9578db48d653ccd15184e4d4e4cac4200ac08
lucas Hervier <lucas.hervier@irt-saintexupery.com>
2023-07-10 10:34:28 +0200

    Bump version: 0.0.1 → 0.0.2

--- deel/libname/__init__.py
+++ deel/libname/__init__.py
@@ -3,6 +3,6 @@
 # CRIAQ and ANITI - https://www.deel.ai/
 # =====================================================================================
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .fake import hello_world, addition
--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     # Name of the package:
     name="lib-name",
     # Version of the package:
-    version="0.0.1",
+    version="0.0.2",
     # Find the package automatically (include everything):
     packages=find_namespace_packages(include=["deel.*"]),
     # Author information:
